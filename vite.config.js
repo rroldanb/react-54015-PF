091910a5034ea814d60f5dc60f6ff494340ac84e
Ruben Roldan <ruben.roldan.b@gmail.com>
2024-01-23 02:45:09 -0300

    acomoda parametros deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-54015-entrega-1/",
+  base: "/Entrega1-Roldan/",
 })
